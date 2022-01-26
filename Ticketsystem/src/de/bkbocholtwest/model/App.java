2a2e6c32217ede015e4bd45067209fe47fc61a15
Johannes <johannes.haick@gmail.com>
2022-01-26 09:09:48 +0100

    classes

--- Ticketsystem/src/de/bkbocholtwest/model/App.java
+++ Ticketsystem/src/de/bkbocholtwest/model/App.java
@@ -1,10 +1,28 @@
 package de.bkbocholtwest.model;
-
+import java.util.ArrayList;
 public class App {
 
+	
+	public static User activeUser;
+	public int lockCounter;
+	public int attemptCounter;
+	
+	public int[] penaltyArray = {1,5,15,60}; 
+	
+	public ArrayList<User> users = new ArrayList<User>();
+			
+			
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public void login() {
+		
+	}
+	
+	public void lockDeviceFor(int lockTime) {
+		
 	}
 
 }
---
+++ Ticketsystem/src/de/bkbocholtwest/model/Ticket.java
@@ -0,0 +1,5 @@
+package de.bkbocholtwest.model;
+
+public class Ticket {
+
+}
