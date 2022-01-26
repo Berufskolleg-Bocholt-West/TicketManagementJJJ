67e8c5db1ae42cc6e65d88af8ec6efe6a9d4ba4c
Johannes <johannes.haick@gmail.com>
2022-01-26 09:22:14 +0100

    user class

--- Ticketsystem/src/de/bkbocholtwest/model/User.java
+++ Ticketsystem/src/de/bkbocholtwest/model/User.java
@@ -1,5 +1,32 @@
 package de.bkbocholtwest.model;
 
+import java.util.ArrayList;
+
 public class User {
 
+	
+	private int userID;
+	private String username;
+	private boolean locked;
+	private String password;
+	private boolean admin;
+	private Department department;
+	
+	private ArrayList<Ticket> createdTickets = new ArrayList<Ticket>();
+	private ArrayList<Ticket> pendingTickets = new ArrayList<Ticket>();
+	
+	
+	public User(String username, int password, Department department) {
+		
+	}
+	
+	public void acceptTickets() {
+		
+	}
+	
+	public void createTickets() {
+		
+	}
+	
+	
 }
