6f6e97a96d537a4c44e35960de1319515506c214
joann-01 <98312202+JoeyUlbrich@users.noreply.github.com>
2022-01-26 09:30:14 +0100

    Update Login.java

--- Ticketsystem/src/de/bkbocholtwest/view/Login.java
+++ Ticketsystem/src/de/bkbocholtwest/view/Login.java
@@ -3,10 +3,15 @@ package de.bkbocholtwest.view;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JRadioButton;
 
 public class Login {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -38,6 +43,28 @@ public class Login {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Username:");
+		lblNewLabel.setBounds(10, 11, 62, 14);
+		frame.getContentPane().add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(85, 8, 141, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("E-Mail Adress:");
+		lblNewLabel_1.setBounds(10, 37, 77, 14);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(85, 34, 141, 20);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Log into existing account");
+		rdbtnNewRadioButton.setBounds(6, 72, 151, 23);
+		frame.getContentPane().add(rdbtnNewRadioButton);
 	}
-
 }
