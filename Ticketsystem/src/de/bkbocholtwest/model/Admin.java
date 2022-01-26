45373300132a15da08987d01fc22530363a8926d
JuFrBK <julian.frieling@bkbocholt-west.eu>
2022-01-26 09:30:04 +0100

    Admin

--- Ticketsystem/src/de/bkbocholtwest/model/Admin.java
+++ Ticketsystem/src/de/bkbocholtwest/model/Admin.java
@@ -1,5 +1,9 @@
 package de.bkbocholtwest.model;
 
 public class Admin extends User{
+	
+	public Admin(String username, int password, Department department) {
+		super(username, password, department);
+	}
 
 }
