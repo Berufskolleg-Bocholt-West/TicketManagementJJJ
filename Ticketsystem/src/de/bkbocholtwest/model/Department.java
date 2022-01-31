dfa2b1088ce508f0e4360a366f1a156b98337155
JuFrBK <julian.frieling@bkbocholt-west.eu>
2022-01-31 08:25:59 +0100

    Department

--- Ticketsystem/src/de/bkbocholtwest/model/Department.java
+++ Ticketsystem/src/de/bkbocholtwest/model/Department.java
@@ -1,5 +1,18 @@
 package de.bkbocholtwest.model;
 
-public class Department {
+import java.util.ArrayList;
 
+
+
+public class Department {
+	
+	private ArrayList<User> members = new ArrayList<User>();
+	
+	private String name;
+	
+	private ArrayList<Ticket> tickets = new ArrayList<Ticket>();
+	
+	public Department() {
+		
+	}
 }
