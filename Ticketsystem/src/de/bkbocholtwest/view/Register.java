37be01b1f84230665866f19303fa86e8fc326f88
Johannes <johannes.haick@gmail.com>
2022-02-09 09:01:54 +0100

    lbl register added

--- Ticketsystem/src/de/bkbocholtwest/view/Register.java
+++ Ticketsystem/src/de/bkbocholtwest/view/Register.java
@@ -14,6 +14,8 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JPasswordField;
+import javax.swing.SwingConstants;
+import java.awt.Font;
 
 public class Register {
 
@@ -79,6 +81,19 @@ public class Register {
 		btnLogin.setBounds(148, 171, 218, 28);
 		frame.getContentPane().add(btnLogin);
 		
+		JLabel lblRegister = new JLabel("The user was successful created.");
+		lblRegister.setHorizontalAlignment(SwingConstants.CENTER);
+		lblRegister.setBounds(148, 223, 218, 47);
+		lblRegister.setVisible(false);
+		frame.getContentPane().add(lblRegister);
+		
+		JLabel lblRegisterUnsuccessful = new JLabel("Username and Password cannot be empty.");
+		lblRegisterUnsuccessful.setHorizontalAlignment(SwingConstants.CENTER);
+		lblRegisterUnsuccessful.setFont(new Font("Tahoma", Font.BOLD, 10));
+		lblRegisterUnsuccessful.setBounds(148, 223, 218, 47);
+		lblRegisterUnsuccessful.setVisible(false);
+		frame.getContentPane().add(lblRegisterUnsuccessful);
+		
 		JButton btnRegister = new JButton("Register");
 		btnRegister.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -88,8 +103,15 @@ public class Register {
 				*/
 				
 				Admin ad = new Admin();
-				
-				ad.createUser(txtUsername.getText(), passwordField.getText());
+				if(!txtUsername.getText().isEmpty() && !passwordField.getText().isEmpty()) {
+					ad.createUser(txtUsername.getText(), passwordField.getText());
+					lblRegisterUnsuccessful.setVisible(false);
+					lblRegister.setVisible(true);
+					lblRegister.grabFocus();
+				}else {
+					lblRegister.setVisible(false);
+					lblRegisterUnsuccessful.setVisible(true);
+				}
 
 				
 			}
@@ -100,5 +122,6 @@ public class Register {
 		passwordField = new JPasswordField();
 		passwordField.setBounds(148, 87, 218, 28);
 		frame.getContentPane().add(passwordField);
+		
 	}
 }
